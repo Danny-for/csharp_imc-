1c029c92a754df6c6133916cd216515b70486ecd
danny-for <dannygeovanna2019@gmail.com>
2022-04-04 22:36:41 -0300

    Csharp_imc

---
+++ Exercicioimc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicioimc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+            float peso;
+            float imc;
+
+            Console.WriteLine("Seja bem vinde !!!");
+
+            Console.WriteLine("Digite sua altura : ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu peso : ");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura ) ;
+
+            Console.WriteLine("Hey Seu imc é de " + imc);
+
+
+        }
+    }
+}
